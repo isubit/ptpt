bd008387da22092212129e5b2111a7d3dc1fe5e5
johnwildspring <john@wildspring.co>
2019-11-11 17:26:50 -0800

    welcome modal stable

--- src/components/modals/WelcomeModal.jsx
+++ src/components/modals/WelcomeModal.jsx
@@ -19,19 +19,22 @@ export class WelcomeModal extends React.Component {
             <React.Fragment>
                 <div className={isDisplayed ? "WelcomeModal active grid-row" : "WelcomeModal grid-row"}>
                     <div className="grid-wrap">
-                        <img className="CloseButton" onClick={this.toggleWelcomeModal} src="../../assets/close_dropdown.svg"></img>
+                        <Link to="/"><img className="CloseButton" onClick={this.toggleWelcomeModal} src="../../assets/close_dropdown.svg"></img></Link>
                         <h2 className="modal-header">Welcome to the Prairie & Tree Planting Tool</h2>
                         <p className="modal-text">To get started, you can use your current location, 
                             add a location or address in the bar above. You 
                             can also pan to an area on the map. Once you have 
                             your location on the map, use the planting tools in 
                             the upper right side to plant your trees and prairies. 
-                            If you need additional help, you can read the help <Link to="/help">help documentation</Link>.
+                            If you need additional help, you can read the <Link to="/help">help documentation</Link>.
                         </p>
                         <div>
                             <span className="modal-link">Dismiss helper popups</span>
-                            <div className="Button">
-                                <span>Let's Get Started</span>
+                            <div className="button-wrap">
+                                <div className="Button">
+                                    <span>Let's Get Started</span>
+                                </div>
+                                <span>Or <Link to="#">Upload a saved file</Link></span>
                             </div>
                         </div>
                     </div>
