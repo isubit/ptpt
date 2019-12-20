9dc0b51c2fa27ac024b5dd66af2897e50f136660
johnwildspring <john@wildspring.co>
2019-12-20 01:27:44 -0500

    finish enrichment

--- src/utils/enrichment.js
+++ src/utils/enrichment.js
@@ -1,10 +1,9 @@
 import calcArea from '@turf/area';
-// import calcIntersect from '@turf/intersect';
 import _ from 'lodash';
 import Debug from 'debug';
 
 import csrRent from 'references/csr_rent.json';
-// import soilSeriesClassification from 'references/soil_series_classification.json';
+import soilSeriesClassification from 'references/soil_series_classification.json';
 
 import {
 	getPolygonCounty,
@@ -17,62 +16,6 @@ import { getTreeRows } from './sources';
 import Worker from './ssurgo.worker.js';
 
 const debug = Debug('Enrichment');
-
-// export async function enrichment(feature, map) {
-// 	const clone = _.cloneDeep(feature);
-
-// 	return {
-// 		boundingTreePolygon() {
-// 			return clone.properties.type === 'tree' && clone.properties.config ? linesToPolygon(getTreeRows({
-// 				...clone,
-// 				properties: {
-// 					...clone.properties,
-// 					config: {
-// 						...(clone.properties.config || {}),
-// 						propagation: 'N', // Placeholder, because it doesn't really matter, the width of the number of rows isn't large enough to span multiple ssurgo areas.
-// 					}
-// 				}
-// 			})) : null;
-// 		},
-// 		acreage() {
-// 			return calcArea(this.boundingTreePolygon() || clone) * 0.000247105;
-// 		},
-// 		async countyRent() {
-// 			let county = null;
-// 			let rent = null;
-// 			try {
-// 				county = await getPolygonCounty(clone);
-// 				rent = county ? csrRent[county] || null : null;
-// 			} catch (e) {
-// 				debug('There was an issue quering polygon county.');
-// 			}
-// 			return {
-// 				county,
-// 				rent,
-// 			};
-// 		},
-// 		ssurgo() {
-// 			const bbox = calcBbox(this.boundingTreePolygon() || clone);
-// 			const ssurgo = map.queryRenderedFeatures([[bbox[0], bbox[1]], [bbox[2], bbox[3]]], {
-// 				layers: ['ssurgo'],
-// 			});
-// 			console.log(ssurgo);
-
-// 			let series = [];
-// 			let csr = [];
-// 			if (ssurgo && ssurgo.length > 0) {
-// 				series = ssurgo.map(ea => ea.properties.compname);
-// 				csr = ssurgo.map(ea => ea.properties.iacornsr);
-// 			}
-
-// 			return {
-// 				series,
-// 				csr,
-// 			};
-// 		},
-// 	};
-// }
-let count = 0;
 
 async function findSSURGOIntersects(feature, mapunits) {
 	const worker = new Worker();
@@ -89,36 +32,14 @@ async function findSSURGOIntersects(feature, mapunits) {
 
 		worker.onmessage = (event) => {
 			if (event.data.intersects) {
-				console.log('intersects:', event.data.intersects);
 				resolve(event.data.intersects);
-			} else {
-				count += 1;
-				console.log('count:', count, event.data);
 			}
 		};
-
-		// function compute() {
-		// 	if (mapunits.length === 0) {
-		// 		console.timeEnd('intersects');
-		// 		resolve(intersects);
-		// 	} else {
-		// 		const mapunit = mapunits.shift();
-		// 		const intersect = calcIntersect(mapunit, feature);
-		// 		// console.log(mapunits.length, mapunit, intersect);
-		// 		intersect && intersects.push(intersect);
-		// 		setImmediate(compute);
-		// 	}
-		// 	return true;
-		// }
-
-		// console.time('intersects');
-		// compute();
 	});
 }
 
 export async function enrichment(feature, map) {
 	debug('Enriching:', feature);
-	count = 0;
 	const clone = _.cloneDeep(feature);
 
 	clone.properties = clone.properties || {};
@@ -180,8 +101,7 @@ export async function enrichment(feature, map) {
 	const ssurgo = map.querySourceFeatures('ssurgo', {
 		sourceLayer: 'default',
 	});
-	console.log(ssurgo);
-	// .filter(ea => calcIntersect(ea, boundingPolygon || clone));
+		// .filter(ea => calcIntersect(ea, boundingPolygon || clone));
 
 	// async function stagger() {
 	// 	return new Promise(resolve => {
@@ -209,19 +129,20 @@ export async function enrichment(feature, map) {
 	// 		run();
 	// 	});
 	// }
-
 	// const intersects = await stagger();
-	const intersects = await findSSURGOIntersects(boundingPolygon || clone, ssurgo);
-	console.log(intersects);
 
-	// if (ssurgo && ssurgo.length > 0) {
-	// 	clone.properties = {
-	// 		...clone.properties,
-	// 		// eslint-disable-next-line no-confusing-arrow
-	// 		series: ssurgo.map(ea => soilSeriesClassification[ea.properties.compname] ? [ea.properties.compname, soilSeriesClassification[ea.properties.compname]] : null).filter(ea => !!ea),
-	// 		csr: ssurgo.map(ea => ea.properties.iacornsr).filter(ea => !!ea),
-	// 	};
-	// }
+	const intersects = await findSSURGOIntersects(boundingPolygon || clone, ssurgo);
+	const ssurgoIntersects = intersects.map(ea => ssurgo.find(mapunit => mapunit.properties.OBJECTID === ea));
+
+	if (ssurgo && ssurgo.length > 0) {
+		clone.properties = {
+			...clone.properties,
+			ssurgo_intersect_data: ssurgoIntersects.map(ea => ea.properties),
+			// eslint-disable-next-line no-confusing-arrow
+			series: ssurgoIntersects.map(ea => soilSeriesClassification[ea.properties.compname] ? [ea.properties.compname, soilSeriesClassification[ea.properties.compname]] : null).filter(ea => !!ea),
+			csr: ssurgoIntersects.map(ea => ea.properties.iacornsr).filter(ea => !!ea),
+		};
+	}
 
 	return clone;
 }
