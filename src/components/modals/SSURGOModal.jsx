1aba684bddb32ba44bd3afa66bbd6edbb7434503
johnwildspring <john@wildspring.co>
2020-04-16 14:55:26 -0700

    edited ssurgo popup

--- src/components/modals/SSURGOModal.jsx
+++ src/components/modals/SSURGOModal.jsx
@@ -2,11 +2,6 @@ import React from 'react';
 import mapboxgl from 'mapbox-gl';
 
 import calcCenter from '@turf/center';
-// this is will update depending on the 'active_ssurgo_feature' that is stored in context
-// on exit delete the 'active_ssurgo_feature' stored in context
-// this will be a popup -- new mapboxgl.Popup()
-
-// create a new popup and populate it with the data from the active SSURGO feature that was set in the map component
 
 export class SSURGOModal extends React.Component {
 	componentDidMount() {
